c1368c2f922c1e408b12d06f65b0697e5e274a6e
Alessandro Faqueti <42614822+afaqueti@users.noreply.github.com>
2019-10-30 17:07:55 -0300

    Delete Mdb-aula3.js

--- Mdb-aula3.js
+++
@@ -1,7 +0,0 @@
-db.meuspokemons.find()
-var query = {name:'Raichu'}
-var fields = {name:1,description:1,attack:1, _id:0} /*A variavel fields que representa 'CAMPOS' ira retornar
-                                                     a quantidade de campos que serao retornados. O valor
-                                                     zero tem como objetivo nao retornar o id do objeto*/
-db.meuspokemons.find(query,fields)
-

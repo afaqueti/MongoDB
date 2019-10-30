8cb54e4d7fd8f7c0bb33a4ba13a40228045b2866
afaqueti <afaqueti@gmail.com>
2019-10-30 16:37:44 -0300

    insert comand fields

--- Mdb-aula3.js
+++ Mdb-aula3.js
@@ -1,2 +1,7 @@
+db.meuspokemons.find()
 var query = {name:'Raichu'}
-db.meuspokemons.find(query)
+var fields = {name:1,description:1,attack:1, _id:0} /*A variavel fields que representa 'CAMPOS' ira retornar
+                                                     a quantidade de campos que serao retornados. O valor
+                                                     zero tem como objetivo nao retornar o id do objeto*/
+db.meuspokemons.find(query,fields)
+

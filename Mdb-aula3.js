472c072cdfd38b6a0bb05533e4a9d59a23d8ee00
afaqueti <afaqueti@gmail.com>
2019-10-31 00:22:56 -0300

    condicao opradores

--- Mdb-aula3.js
+++ Mdb-aula3.js
@@ -7,8 +7,9 @@ var fields = {name:1,description:1,attack:1, _id:0} /*A variavel fields que repr
                                                      zero tem como objetivo nao retornar o id do objeto*/
 db.meuspokemons.find(query,fields)
 
-/*
+
 ================== OPERADORES CONDICIONAIS =========================
+/*
 PARA CONDIÇAO < menor
 DEVEMOS USAR $lt que representa (less than) - Menor que
 
@@ -16,7 +17,7 @@ PARA CONDIÇAO <= menor ou igual
 DEVEMOS USAR $lte que representa (less than or equal) - Menor ou igual
 
 PARA CONDIÇAO <= maior
-DEVEMOS USAR $gt que representa (great than) - Maior 
+DEVEMOS USAR $gt que representa (great than) - Maior que
 
 PARA CONDIÇAO <= maior ou igual
 DEVEMOS USAR $gte que representa (great than or equal) - Maior ou igual
@@ -27,4 +28,11 @@ var qry = {attack: {$lte: '40'}} /*Consulta executada usando a variavel qry, pro
                                   alteraçao somente dos valores.*/
 qry
 db.meuspokemons.find(qry)
+
+================== OPERADORES LOGICOS =========================
+db.meuspokemons.find()
+var query = {$or:[{name:'Raichu'},{heigth:'0.3'}]} /*O operados OR ira aceitar um array de obejtos, portanto ele ira retornar objetos verdadeiros
+                                                   ou o nome ou a altura, conforme valor do objeto.*/
+query
+db.meuspokemons.find(query) /*O parametro QEURY ira usar os objetos conforme a variavel definida*/
 
